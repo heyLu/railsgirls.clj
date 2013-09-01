a95d7ce8cefc9886a0457ceda8365c63bdc514ef
Lucas Stadler <mi.au@papill0n.org>
2013-09-01 15:54:06 +0200

    Change template name to 'idea'.
    Took quite long. Obviously that tiny comment is important.

--- app/src/app/clj/html_templates.clj
+++ app/src/app/clj/html_templates.clj
@@ -1,8 +1,8 @@
 (ns app.clj.html-templates
   (:use [io.pedestal.app.templates :only [tfn dtfn tnodes]]))
 
+;; Note: this file will not be reloaded automatically when it is changed.
+
 (defmacro app.clj-templates
   []
-  {:app.clj-page (dtfn (tnodes "app.clj.html" "hello") #{:id})})
-
-;; Note: this file will not be reloaded automatically when it is changed.
+  {:app.clj-page (dtfn (tnodes "app.clj.html" "idea") #{:id})})
